463732b7921947285922e4a498c8a029fcd77559
Gabriel Lacerda Lemos <gabriellemoslacerda@gmail.com>
2026-02-24 00:59:26 -0300

    Lab01S01: Planejamento e Estrutura Nuxt

---
+++ app/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="bg-slate-950 text-white">
+    <Navigation />
+    <Hero />
+    <Projects />
+    <Skills />
+    <About />
+    <Contacts />
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: 'Portfolio',
+  meta: [
+    { name: 'description', content: 'Software Engineering Portfolio' }
+  ]
+})
+</script>
